fc32f126ac617c4c5599330d4a2f79a480726541
Anton Bezmelnitsin <a.bezmelnitsin.a@yandex.ru>
2024-02-16 17:18:17 +0500

    Part 6.2
    Layout using Bootstrap classes

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -1,12 +1,17 @@
 package com.example.demo.model;
 
+import java.util.Date;
+
 public class Post {
 
-    private String text;
+    final private String text;
     private Integer likes;
+    final private Date creationDate;
 
-    public Post(String text) {
+    public Post(String text, Date creationDate) {
         this.text = text;
+        this.creationDate = creationDate;
+        this.likes = 0;
     }
 
     public String getText() {
@@ -15,5 +20,9 @@ public class Post {
 
     public Integer getLikes() {
         return likes;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
     }
 }
--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -4,6 +4,7 @@ import com.example.demo.model.Post;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -11,9 +12,9 @@ public class PostService {
 
     public List<Post> listAllPosts() {
         List<Post> posts = new ArrayList<Post>();
-        posts.add(new Post("1-st post"));
-        posts.add(new Post("2-nd post"));
-        posts.add(new Post("3-rd post"));
+        posts.add(new Post("1-st post", new Date()));
+        posts.add(new Post("2-nd post", new Date()));
+        posts.add(new Post("3-rd post", new Date()));
 
         return posts;
     }
